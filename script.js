d82a985aefc04adf68f18599905aea1166223b6f
Champ Henry Duco <ducochamp@gmail.com>
2023-08-15 14:13:12 +0800

    right portfolio

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll("nav a");
-    
-    navLinks.forEach(link => {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const targetSectionId = link.getAttribute("href");
-            const targetSection = document.querySelector(targetSectionId);
-            const offset = targetSection.getBoundingClientRect().top;
-            const headerHeight = document.querySelector("header").offsetHeight;
-            const scrollPosition = offset + window.scrollY - headerHeight;
-            
-            window.scrollTo({
-                top: scrollPosition,
-                behavior: "smooth"
-            });
-        });
-    });
-});
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+}
